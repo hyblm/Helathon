3b0f43953f427e8d93d3a8752b092b955272a5fa
hyblm <hyma.code@gmail.com>
2024-11-12 15:14:31 +0100

    footer margin no longer bleads into affecting the main content section

--- frontend/src/components/BaseLayout.vue
+++ frontend/src/components/BaseLayout.vue
@@ -5,13 +5,15 @@ import { Icon } from '@iconify/vue'
 </script>
 
 <template>
-  <div class="min-h-svh flex flex-col">
-    <header class="flex gap-3 p-5 items-center justify-between">
+  <div class="flex min-h-svh flex-col">
+    <header class="flex items-center justify-between gap-3 p-5">
       <p class="text-lg font-black tracking-tight">ELMM™</p>
       <ModeToggle />
     </header>
-    <slot />
-    <footer class="mt-auto p-5 flex gap-3 items-center">
+    <div class="container flex h-full flex-grow">
+      <slot />
+    </div>
+    <footer class="flex items-center gap-3 p-5">
       <a href="https://github.com/hyblm/hellathon-4">
         <Button variant="secondary">
           <Icon icon="lineicons:github" />

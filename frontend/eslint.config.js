7baf593a4d79e1eb5f04071648a17fcbee70d638
hyblm <hyma.code@gmail.com>
2024-11-12 19:04:18 +0100

    configure eslint for typescript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,10 +1,17 @@
 import js from '@eslint/js'
+import ts from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
 
-export default [
+export default ts.config(
   js.configs.recommended,
+  ...ts.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
-    rules: {},
-  },
-]
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  }
+)
